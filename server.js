88d61de3a7f4aaeea1e7d55e2a916008d64e183b
beaglepuppy88 <atieu@yahoo.com>
2018-08-16 20:52:51 -0400

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-
-
-
 
 app.use((req, res, next) =>  {
   var now = new Date().toString();
@@ -25,9 +24,11 @@ app.use((req, res, next) =>  {
   next();
 });
 
+/*
 app.use((req, res, next) => {
   res.render('maintenance.hbs');
 });
+*/
 
 app.use(express.static(__dirname + '/public'));
 
